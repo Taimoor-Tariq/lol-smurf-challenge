cfac31f2675163d4e9f1695fd0da57f725346c59
a054132d-f76c-4ef6-8de4-75287df54e56 <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-12-27 04:26:37 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const config = require('./config');
-const schedule = require('node-schedule');
-const rp = require('request-promise');
 const db = require('./database');
 const express = require('express');
 const app = express();
@@ -11,45 +9,3 @@ const app = express();
 //     });
 // });
 
-let getGames = (plr) => {
-    plrGames = JSON.parse(plr.GAMES);
-    rp.get(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${plr.ID}?queue=420&beginIndex=${plr.LAST_RECORDED}&api_key=${config.RIOT_API_KEY}`)
-        .then(res => {
-            res = JSON.parse(res);
-            let prom = res.matches.map(m => {
-                plrGames.push({
-                    id: m.gameId,
-                    lane: m.lane,
-                    time: m.timestamp,
-                    champ: m.champion
-                });
-            });
-
-            Promise.all(prom).then(() => {
-
-                db.updateGames(plr.ID, JSON.stringify(plrGames));
-                db.updateLastRecorded(plr.ID, res.totalGames);
-            })
-        }).catch(err => {
-            err = JSON.parse(err.error);
-            switch (err.status.status_code) {
-                case 400:
-                    db.fixID(plr.SUMMONER_NAME).then(newID => {
-                        plr.ID = newID;
-                        getGames(plr);
-                    });
-                    break;
-
-                case 404:
-                    break;
-            
-                default:
-                    console.error(err);
-                    break;
-            }
-        });
-}
-
-let main = async () => {db.getPlrs().then(plrs => plrs.map(p => getGames(p)))}
-
-main();
